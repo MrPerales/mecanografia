003635c55ec6be059392e78d0189fe734830a989
Carlos Perales <carlosperales050@gmail.com>
2025-02-24 16:38:02 -0600

    divicion de palabras y letras

---
+++ index.js
@@ -0,0 +1,33 @@
+const $ = (element) => document.querySelector(element);
+
+const $time = $("time");
+const $paragraph = $("p");
+const $input = $("input");
+
+const INITIAL_TIME = 10;
+const TEXT =
+  "Estas palabras abarcan una buena variedad de letras combinaciones y elementos gramaticales sin repetirse Espero que te sean útiles para practicar mecanografía";
+
+let words = [];
+let currentTime = INITIAL_TIME;
+
+initGame();
+initEvents();
+
+function initGame() {
+  words = TEXT.split(" ").slice(0, 20);
+  currentTime = INITIAL_TIME;
+  $time.textContent = currentTime;
+  //dividimos las palabras y las letras en diferentes etiquetas
+  //etiquetas sin semantica
+  $paragraph.innerHTML = words
+    .map((word, index) => {
+      // separamos la letra => hola => h o l a
+      const letters = word.split("");
+      return `<x-word>
+        ${letters.map((letter) => `<x-latter>${letter}</x-latter>`).join("")}
+        </x-word>`;
+    })
+    .join("");
+}
+function initEvents() {}
