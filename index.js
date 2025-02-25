c1d618f2b289a13525e52d956b8667a32a55966b
Carlos Perales <carlosperales050@gmail.com>
2025-02-24 19:33:12 -0600

    check letter to letter

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const $input = $("input");
 
 const INITIAL_TIME = 10;
 const TEXT =
-  "Estas palabras abarcan una buena variedad de letras combinaciones y elementos gramaticales sin repetirse Espero que te sean útiles para practicar mecanografía";
+  "estas palabras abarcan una buena variedad de letras combinaciones y elementos gramaticales sin repetirse Espero que te sean útiles para practicar mecanografía";
 
 let words = [];
 let currentTime = INITIAL_TIME;
@@ -25,14 +25,14 @@ function initGame() {
       // separamos la letra => hola => h o l a
       const letters = word.split("");
       return `<x-word>
-        ${letters.map((letter) => `<x-latter>${letter}</x-latter>`).join("")}
+        ${letters.map((letter) => `<x-letter>${letter}</x-letter>`).join("")}
         </x-word>`;
     })
     .join("");
   //seleccionamos la primera palabra y letra para pintar el cursor "|"
   const $firstWord = $paragraph.querySelector("x-word");
   $firstWord.classList.add("active");
-  $firstWord.querySelector("x-latter").classList.add("active");
+  $firstWord.querySelector("x-letter").classList.add("active");
 
   const interval = setInterval(() => {
     currentTime--;
@@ -43,7 +43,40 @@ function initGame() {
     }
   }, 1000);
 }
-function initEvents() {}
+function initEvents() {
+  document.addEventListener("click", () => {
+    $input.focus();
+  });
+  document.addEventListener("keydown", onKeyDown);
+  document.addEventListener("keyup", onKeyUp);
+}
+function onKeyDown() {}
+function onKeyUp() {
+  // recuperamos elementos actuales
+  const $currentWord = $paragraph.querySelector("x-word.active");
+  const $currentLetter = $currentWord.querySelector("x-letter.active");
+  // recuperamos la primera palabra para poder comparar
+  const currentWord = $currentWord.innerText.trim();
+  $input.maxLength = currentWord.length; //limitamos numero de letras
+
+  // recuperamos todas las letras
+  const $allLetters = $currentWord.querySelectorAll("x-letter");
+  // limpiamos las clases , para poder repintar si retrocedemos una letra
+  $allLetters.forEach(($letter) =>
+    $letter.classList.remove("correct", "incorrect")
+  );
+  //convertimos el valor del input en array para que podamos compara letra por letra
+  $input.value.split("").forEach((item, index) => {
+    const $letter = $allLetters[index];
+    const letterToCheck = currentWord[index];
+    // agregamos clase si coincide
+    if (item === letterToCheck) {
+      $letter.classList.add("correct");
+    } else {
+      $letter.classList.add("incorrect");
+    }
+  });
+}
 function gameOver() {
   console.log("GameOver");
 }
